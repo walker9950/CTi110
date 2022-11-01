c56272b79fff3d1cc55fc8eeb330453624674beb
walker9950 <117189352+walker9950@users.noreply.github.com>
2022-11-01 11:46:38 -0400

    Create P1Hw.py

---
+++ P1Hw.py
@@ -0,0 +1,18 @@
+# CTI 110
+# P1HW1
+# simple Input Output
+
+# ask the user for a number
+print (" please enter  a number :")
+num = int( input() )
+
+print ("your number is ", num)
+
+print("that number squared is",num *num)
+
+#print num + 5
+print (num + 5)
+# print num *5
+print (num * 5)
+
+
