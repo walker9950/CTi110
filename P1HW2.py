7dfa51b930e39da5aa6111be17ec7135f89d3dca
walker9950 <117189352+walker9950@users.noreply.github.com>
2022-11-03 11:37:59 -0400

    Add files via upload

---
+++ P1HW2.py
@@ -0,0 +1,59 @@
+
+
+# CTI 110
+# P1H2 - Expenses
+
+
+#$ input: three expenses, the budget
+# processing: total the expenses compare with budget
+# output: did you go over or under
+
+
+# our variables - type float, because of the decimal place
+gas = 0.0
+food = 0.0
+hotel =0.0
+totalExpenses = 0.0 # gas + food hotel
+budget = 0.0
+destination = ""
+
+print (" this prograam calculates on displays travel expenses")
+
+print (" Enter your budget: ")
+budget = float(input())
+
+destination = input ("where are you heading?")
+
+print("ok,let's total your expenses.")
+print(" gas: $")
+gas = float(input())
+
+
+print("hotel: $")
+hotel = float(input())
+
+
+print("food:$")
+food = float(input())
+
+# add everything up
+print("your total expenses are: $", totoal expenses)
+print("your budget was:,bubget)
+      
+      
+      if totalExpenses > budget:
+ else:
+       print("you stayed within your budget.") 
+      
+      print("ok,let's total your expenses.")
+      
+      gas =float (input("gas:$"))
+      hotal = float(input("Hotel"))
+      food = float(input("food:"))
+      
+# add every thing up
+ totalExpenses = gas + hotal + food
+ # output - did they go over budget?
+ print ("your total expenses are: $", totalExpenses)
+ print("your budget was: $,budget)
+        
